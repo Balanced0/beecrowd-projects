93b35b2cc5e18342454ffe3618a0606b4be9108a
Balanced0 <167624337+Balanced0@users.noreply.github.com>
2024-07-06 00:47:21 +0600

    Create 1012-Area.java

---
+++ projects/BEGINNER/1012-Area.java
@@ -0,0 +1,47 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 import java.io.IOException;\
+import java.util.Scanner;\
+/**\
+ * IMPORTANT: \
+ *      O nome da classe deve ser "Main" para que a sua solu\'e7\'e3o execute\
+ *      Class name must be "Main" for your solution to execute\
+ *      El nombre de la clase debe ser "Main" para que su soluci\'f3n ejecutar\
+ */\
+public class Main \{\
+ \
+    public static void main(String[] args) throws IOException \{\
+ \
+        /**\
+         * Escreva a sua solu\'e7\'e3o aqui\
+         * Code your solution here\
+         * Escriba su soluci\'f3n aqu\'ed\
+         */\
+         Scanner sc = new Scanner(System.in);\
+         double A = sc.nextDouble();\
+         double B = sc.nextDouble();\
+         double C = sc.nextDouble();\
+         double triangle = 0.5 * A * C;\
+         double circle = 3.14159 * C * C;\
+         double trapezium = 0.5 * (A + B) * C;\
+         double square = B * B;\
+         double rectangle = A * B;\
+         String TRIANGLE = String.format("%.3f", triangle);\
+         String CIRCLE = String.format("%.3f", circle);\
+         String TRAPEZIUM = String.format("%.3f", trapezium);\
+         String SQUARE = String.format("%.3f", square);\
+         String RECTANGLE = String.format("%.3f", rectangle);\
+         System.out.println("TRIANGULO: " + TRIANGLE);\
+         System.out.println("CIRCULO: " + CIRCLE);\
+         System.out.println("TRAPEZIO: " + TRAPEZIUM);\
+         System.out.println("QUADRADO: " + SQUARE);\
+         System.out.println("RETANGULO: " + RECTANGLE);\
+ \
+    \}\
+ \
+\}}
