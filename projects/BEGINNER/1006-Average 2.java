f01ab3486f0639e6d1d43abdc111de951bd4e086
Balanced0 <167624337+Balanced0@users.noreply.github.com>
2024-07-05 21:43:29 +0600

    Create 1006-Average 2.java

---
+++ projects/BEGINNER/1006-Average 2.java
@@ -0,0 +1,38 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 import java.util.Scanner;\
+ \
+/**\
+ * IMPORTANT: \
+ *      O nome da classe deve ser "Main" para que a sua solu\'e7\'e3o execute\
+ *      Class name must be "Main" for your solution to execute\
+ *      El nombre de la clase debe ser "Main" para que su soluci\'f3n ejecutar\
+ */\
+public class Main \{\
+ \
+    public static void main(String[] args) throws IOException \{\
+ \
+        /**\
+         * Escreva a sua solu\'e7\'e3o aqui\
+         * Code your solution here\
+         * Escriba su soluci\'f3n aqu\'ed\
+         */\
+         Scanner sc = new Scanner(System.in);\
+         double A = sc.nextDouble();\
+         double B = sc.nextDouble();\
+         double C = sc.nextDouble();\
+         double weight_A = 2.0;\
+         double weight_B = 3.0;\
+         double weight_C = 5.0;\
+         double avg = (A * weight_A + B * weight_B + C * weight_C)/(weight_A + weight_B + weight_C);\
+         String MEDIA = String.format("%.1f", avg);\
+         System.out.println("MEDIA = " + MEDIA);\
+ \
+    \}\
+ \
+\}}
