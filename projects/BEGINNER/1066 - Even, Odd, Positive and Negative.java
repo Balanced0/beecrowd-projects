16521c5c643eec1358377f7b3d5db3661bfded30
Balanced0 <167624337+Balanced0@users.noreply.github.com>
2024-08-09 18:57:18 +0600

    1066 - Even, Odd, Positive and Negative

---
+++ projects/BEGINNER/1066 - Even, Odd, Positive and Negative.java
@@ -0,0 +1,55 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 import java.io.IOException;\
+import java.util.Scanner;\
+ \
+/**\
+ * IMPORTANT: \
+ *      O nome da classe deve ser "Main" para que a sua solu\'e7\'e3o execute\
+ *      Class name must be "Main" for your solution to execute\
+ *      El nombre de la clase debe ser "Main" para que su soluci\'f3n ejecutar\
+ */\
+public class Main \{\
+ \
+    public static void main(String[] args) throws IOException \{\
+ \
+        /**\
+         * Escreva a sua solu\'e7\'e3o aqui\
+         * Code your solution here\
+         * Escriba su soluci\'f3n aqu\'ed\
+         */\
+         Scanner sc = new Scanner(System.in);\
+         int ecount = 0;\
+         int ocount = 0;\
+         int pcount = 0;\
+         int ncount = 0;\
+         for(int i = 1; i <= 5; i++)\{\
+             int n = sc.nextInt();\
+             if(n%2 == 0)\{\
+                 ecount++;\
+             \}\
+             if(n%2 != 0)\{\
+                 ocount++;\
+             \}\
+             if(n > 0)\{\
+                 pcount++;\
+             \}\
+             if(n < 0)\{\
+                 ncount++;\
+             \}\
+             \
+         \}\
+         System.out.println(ecount + " valor(es) par(es)");\
+         System.out.println(ocount + " valor(es) impar(es)");\
+         System.out.println(pcount + " valor(es) positivo(s)");\
+         System.out.println(ncount + " valor(es) negativo(s)");\
+         sc.close();\
+ \
+    \}\
+ \
+\}}
